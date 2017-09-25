e918cc2237ef2e23137c9f279614bcfacb7ad0bb
Jothipriya Shekhar <jotitan27@gmail.com>
2017-09-25 12:03:06 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+     age : number;
+     message : string = 'Know your age by selecting your Date Of Birth';
+
+  onCalculate(bday){
+           let timeDiff = Math.abs(Date.now() - new Date(bday).getTime());
+            console.log('diff: ', timeDiff)
+            this.age = Math.floor((timeDiff / (1000 * 3600 * 24))/365);
+            console.log(this.age)
+  }
 }
